4e52ace4513ed09e4b5fbd4be7cd935692b45a44
vignesh <eurekaithead@gmail.com>
2025-11-03 20:33:15 +0530

    remove console error

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -18,67 +18,7 @@
 				</a>
 			</div>
 			<!-- /Logo -->
-			<div class="modern-profile p-3 pb-0">
-				<div class="text-center rounded bg-light p-3 mb-4 user-profile">
-					<div class="avatar avatar-lg online mb-3">
-						<img src="{{ asset('assets/img/profiles/avatar-02.jpg') }}" alt="Img" class="img-fluid rounded-circle">
-					</div>
-					<h6 class="fs-12 fw-normal mb-1">Adrian Herman</h6>
-					<p class="fs-10">System Admin</p>
-				</div>
-				<div class="sidebar-nav mb-3">
-					<ul class="nav nav-tabs nav-tabs-solid nav-tabs-rounded nav-justified bg-transparent"
-						role="tablist">
-						<li class="nav-item"><a class="nav-link active border-0" href="#">Menu</a></li>
-						<li class="nav-item"><a class="nav-link border-0" href="#">Chats</a></li>
-						<li class="nav-item"><a class="nav-link border-0" href="#">Inbox</a></li>
-					</ul>
-				</div>
-			</div>
-			<div class="sidebar-header p-3 pb-0 pt-2">
-				<div class="text-center rounded bg-light p-2 mb-4 sidebar-profile d-flex align-items-center">
-					<div class="avatar avatar-md online">
-						<img src="{{ asset('assets/img/profiles/avatar-02.jpg') }}" alt="Img" class="img-fluid rounded-circle">
-					</div>
-					<div class="text-start sidebar-profile-info ms-2">
-						<h6 class="fs-12 fw-normal mb-1">Adrian Herman</h6>
-						<p class="fs-10">System Admin</p>
-					</div>
-				</div>
-				<div class="input-group input-group-flat d-inline-flex mb-4">
-					<span class="input-icon-addon">
-						<i class="ti ti-search"></i>
-					</span>
-					<input type="text" class="form-control" placeholder="Search in HRMS">
-					<span class="input-group-text">
-						<kbd>CTRL + / </kbd>
-					</span>
-				</div>
-				<div class="d-flex align-items-center justify-content-between menu-item mb-3">
-					<div class="me-3">
-						<a href="#" class="btn btn-menubar">
-							<i class="ti ti-layout-grid-remove"></i>
-						</a>
-					</div>
-					<div class="me-3">
-						<a href="#" class="btn btn-menubar position-relative">
-							<i class="ti ti-brand-hipchat"></i>
-							<span class="badge bg-info rounded-pill d-flex align-items-center justify-content-center header-badge">5</span>
-						</a>
-					</div>
-					<div class="me-3 notification-item">
-						<a href="#" class="btn btn-menubar position-relative me-1">
-							<i class="ti ti-bell"></i>
-							<span class="notification-status-dot"></span>
-						</a>
-					</div>
-					<div class="me-0">
-						<a href="#" class="btn btn-menubar">
-							<i class="ti ti-message"></i>
-						</a>
-					</div>
-				</div>
-			</div>
+			
 
 
 			<div class="sidebar-inner slimscroll">
@@ -91,14 +31,14 @@
 							<li class="menu-item">
 								<a href="{{ route('admin.users.index') }}" >
 									<i class="ti ti-users" style="margin-right: 5px;"></i> 
-									<span> Users - Management</span>
+									<span> User Management</span>
 								</a>
 							</li>
 
 							<li class="menu-item">
 								<a href="{{ route('admin.projects.index') }}">
 									<i class="ti ti-briefcase" style="margin-right: 5px;"></i> 
-									<span> Project - Management</span>
+									<span> Project Management</span>
 								</a>
 							</li>
 
@@ -107,7 +47,7 @@
 							<li class="menu-item">
 								<a href="{{ route('admin.projects.index') }}">
 									<i class="ti ti-briefcase" style="margin-right: 5px;"></i> 
-									<span> Project - Management</span>
+									<span> Project Management</span>
 								</a>
 							</li>
 
